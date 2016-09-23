c73adbba167e4519956a5edbcdefbc9a3a8a02e0
Daniel Vicarel <shundra8820@gmail.com>
2016-09-22 21:27:54 -0400

    The P4 script now outputs cleaner log messages

--- p4_master.bash
+++ p4_master.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+echo
+echo Beginning setup of a Perforce Helix master server!
+
 # Define constants
-SERVERNAME=danware                  # should not contain spaces    
-P4SERVERDIR=/opt/perforce/servers	# should not contain spaces
-P4ROOTDIR=$P4SERVERDIR/$serverName    # should not contain spaces
-FILESDIR=$P4ROOTDIR/files             # should not contain spaces
-SSLDIR=$P4ROOTDIR/ssl                 # should not contain spaces
+SERVERNAME=danware                  # must not contain spaces    
+P4SERVERDIR=/opt/perforce/servers	# must not contain spaces
+P4ROOTDIR=$P4SERVERDIR/$SERVERNAME  # must not contain spaces
+FILESDIR=$P4ROOTDIR/files           # must not contain spaces
+SSLDIR=$P4ROOTDIR/ssl               # must not contain spaces
+yum install -y curl > NUL           # should already be installed in Amazon Linux AMIs
 AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 REGION=${AZ:0:-1}
 SERVERDNS=p4.danvicarel.com
@@ -14,11 +18,15 @@ SERVERDNS=p4.danvicarel.com
 echo PS1="\"\\n\$PS1\"">> /home/ec2-user/.bashrc
 
 # Update all packages
-yum update -y
+echo
+echo Updating all YUM packages...
+yum update -y > NUL
 
 # Install Perforce packages
 # The RHEL/7 part of the baseurl should be replaced with
 # the latest RHEL version that both Amazon and Perforce support
+echo
+echo Adding the Perforce YUM repo as follows:
 rpm -import https://package.perforce.com/perforce.pubkey
 tee /etc/yum.repos.d/perforce.repo <<- EOB
 [perforce]
@@ -27,34 +35,39 @@ baseurl=http://package.perforce.com/yum/rhel/7/x86_64
 enabled=1
 gpgcheck=1
 EOB
-yum install -y helix-p4d
+echo
+echo Installing Perforce Helix...
+yum install -y helix-p4d > NUL
 
 # ***** UNCOMMENT IF NECESSARY *****
+# echo Formatting drives...
 # # Format EBS volumes
-# dev1=/dev/sdb     # should not contain spaces
-# dbLbl=p4db        # should not contain spaces
+# dev1=/dev/sdb     # must not contain spaces
+# dbLbl=p4db        # must not contain spaces
 # chkptLbl=p4chkpt
 # parted -s $dev1 mktable gpt
 # parted -s -a optimal $dev1 mkpart primary xfs 0% 100%
 # parted -s $dev1 name 1 $dbLbl
-# yum install -y xfsprogs
+# yum install -y xfsprogs > NUL     # should already be installed in Amazon Linux AMIs
 # mkfs -t xfs "$dev1"1
 # xfs_admin -L $dbLbl "$dev1"1
 
 # Make directories for the server, owned by new "perforce" user
+echo Adding server directories...
 mkdir -p $FILESDIR $SSLDIR
 chown -R perforce:perforce $P4ROOTDIR
 chmod 700 $SSLDIR
 
 # Set up automatic mounts (for this and all future reboots)
-yum install -y curl
+echo Mounting the shared filesystem for versioned files...
 efsID=fs-1768925e
 echo "${AZ}.${efsID}.efs.${REGION}.amazonaws.com:/ $FILESDIR nfs4 defaults,vers=4.1,noresvport 0 0" >> /etc/fstab
-yum install -y nfs-utils    # should already be installed in Amazon Linux AMIs
+yum install -y nfs-utils  > NUL   # should already be installed in Amazon Linux AMIs
 mount -a -t nfs4
 
 # Configure the Perforce server daemon
-journalPath=$P4ROOTDIR/journal.live   # should not contain spaces
+echo Configuring the Danware server daemon as follows:
+journalPath=$P4ROOTDIR/journal.live   # must not contain spaces
 tee /etc/perforce/p4dctl.conf.d/$SERVERNAME.conf <<- EOB
 p4d danware
 {
@@ -76,29 +89,37 @@ p4d danware
 EOB
 
 # Generate the server's private key and certificate
+echo
+echo Configuring SSL certificates for the server as follows:
 tee $SSLDIR/config.txt <<- EOB
-C = US           # Country Name - 2 letter code
-ST = OH          # ST: State or Province Name - full name
-L = Akron        # L: Locality or City Name
-O = Danware      # O: Organization or Company Name
-OU = Software    # OU = Organization Unit - division or unit
-CN = $SERVERDNS  # CN: Common Name (usually the DNS name of the server)
-EX = 730         # EX: number of days from today for certificate expiration
-UNITS = days     # UNITS: unit multiplier for expiration - "secs", "mins", "hours", or "days"
+C = US                  # Country Name - 2 letter code
+ST = OH                 # ST: State or Province Name - full name
+L = Akron               # L: Locality or City Name
+O = Danware             # O: Organization or Company Name
+OU = Software           # OU = Organization Unit - division or unit
+CN = $SERVERDNS         # CN: Common Name (usually the DNS name of the server)
+EX = 730                # EX: number of days from today for certificate expiration
+UNITS = days            # UNITS: unit multiplier for expiration - "secs", "mins", "hours", or "days"
 EOB
 sudo -u perforce p4d -Gc
 
 # Start the service
+echo
+echo Starting the Danware server...
 #sudo -u perforce p4dctl start -t p4d $serverName
 
 # Fire up the local Postfix mail server required for Helix GitSwarm
-yum install -y postfix
-chkconfig postfix on
+echo
+echo Installing postfix for GitSwarm...
+yum install -y postfix > NUL
+echo Starting postfix...
 service postfix start
+chkconfig postfix on
 
 # Install and reconfigure GitSwarm
+echo Installing Perforce Helix GitSwarm...
 gitswarmCfg=/etc/gitswarm/gitswarm.rb
-yum install -y helix-gitswarm
+yum install -y helix-gitswarm > NUL
 sed -r "s|^external_url 'http:\/\/.*'|external_url 'http:\/\/$SERVERDNS'|" $gitswarmCfg | tee $gitswarmCfg
 #adjust time_zone line here
 gitswarm-ctl reconfigure
