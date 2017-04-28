ec619d0d7072728fe7b14f11c1a7a21facfd2c4f
Dan Vicarel <Daniel.Vicarel@amtrustgroup.com>
2017-04-28 17:47:22 -0400

    Closes #27.  Removed the UserData bash scripts

--- shell-scripts/add-newline-to-shell.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo
-echo Adding a new line to the bash prompt...
-echo PS1="\"\\n\$PS1\"">> /home/ec2-user/.bashrc
-echo Success!
--- shell-scripts/set-timezone.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-timeZone=$1
-
-echo
-echo Adjusting time zone to $timeZone...
-sed -ir "s|ZONE=\"UTC\"|ZONE=\"$timeZone\"|" /etc/sysconfig/clock
-ln -sf /usr/share/zoneinfo/$timeZone /etc/localtime
-echo Success!
